59bf499c44f1a1af17d467421c1e88f166cf3704
Zaid Albirawi <zaid@tyk.io>
2020-12-04 13:17:19 -0500

    Add docker override setup for Tyk Pro

--- Readme.md
+++ Readme.md
@@ -4,3 +4,5 @@ Simple demo to connect Tyk Pro and a simple api inside Docker.
 
 ## Getting Started
 1. Run initalization script to install npm packages and clone [tyk-pro-docker-demo](https://github.com/TykTechnologies/tyk-pro-docker-demo) repo `sh scripts/init.sh`.
+
+2. Navigate to the `tyk` folder and run the following script to launch Tyk Pro `docker-compose -f docker-compose.yml -f docker-local.yml up`.
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,16 @@
 cd simple-api
+echo 'Installing node dependencies...'
 npm install
 
 cd ..
-git clone https://github.com/TykTechnologies/tyk-pro-docker-demo.git tyk
+echo 'Cloning Tyk Pro demo repo...'
+npx degit https://github.com/TykTechnologies/tyk-pro-docker-demo.git tyk
+
+echo 'Copying env file...'
+cp scripts/.env.example tyk/.env
+
+echo 'Copying docker-local.yml file...'
+cp scripts/docker-local.yml tyk/docker-local.yml
+
+read -p 'Please enter your Tyk Pro License key: ' license_key
+echo LICENSE_KEY=$license_key >> tyk/.env
