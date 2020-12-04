ab963e5076b41f88f54b4a1d67c79d7729c15a3a
Zaid Albirawi <zaid@tyk.io>
2020-12-04 17:16:00 -0500

    Move init files to init folder

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,13 +2,13 @@ echo 'Cloning Tyk Pro demo repo...'
 npx degit --force https://github.com/TykTechnologies/tyk-pro-docker-demo.git tyk
 
 echo 'Copying simple-api folder...'
-cp -R scripts/simple-api tyk/simple-api
+cp -R scripts/init/simple-api tyk/simple-api
 
 echo 'Copying env file...'
-cp scripts/.env.example tyk/.env
+cp scripts/init/.env.example tyk/.env
 
 echo 'Copying docker-local.yml file...'
-cp scripts/docker-local.yml tyk/docker-local.yml
+cp scripts/init/docker-local.yml tyk/docker-local.yml
 
 read -p 'Please enter your Tyk Pro License key: ' license_key
 echo LICENSE_KEY=$license_key >> tyk/.env
